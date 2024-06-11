cbfd0081aef0a832c77266560b2a6b12be3b7d17
anay10 <anay.damle@gmail.com>
2024-06-11 18:31:29 +0530

    First committ

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function TodoInput(props){
+    const {handleAddTodo, setTodoValue, todoValue} = props
+
+    return (
+        <>
+        <header>
+            <input value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)}} placeholder="Enter todo......" />
+            <button onClick={() => {
+                handleAddTodo(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+        </>
+    );
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import TodoCard from "./TodoCard";
+
+export default function TodoList(props){
+    const {todos, handleDeleteTodo} = props;
+    return (
+        <>
+        <ul className="main">
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard {...props} index={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                );
+            })}
+        </ul>
+        </>
+    );
+}
